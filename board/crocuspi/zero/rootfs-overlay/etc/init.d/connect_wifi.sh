3f0b19deda00985340152bde07b331f83135fbab
CrocusPI <crocuspi@dsyteknoloji.com>
2023-01-15 13:20:27 +0300

    wifi adaptation

--- board/crocuspi/zero/rootfs-overlay/etc/init.d/connect_wifi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ sh
-insmod /usr/lib/esp8089.ko esp_reset_gpio=142#add driver
-sleep 1
-ifconfig wlan0 up #Open wifi
-wpa_supplicant -B -d -i wlan0 -c /etc/wpa_supplicant.conf #search wifi
-sleep 2
-udhcpc -i wlan0 #Connect wifi
